ee44fb34378af913698a64903e80c1a0a638144d
Uriel Aispuro <urielalexis64@gmail.com>
2020-11-25 20:42:41 -0700

    login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { UserModel } from "../../models/user.model";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  user: UserModel;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.user = new UserModel();
   }
 
+  login(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    console.log(this.user);
+    console.log(form);
+  }
 }
